5ca32896888abbfc12c3384500be555b5b574035
tvishnu3111 <145796147+tvishnu3111@users.noreply.github.com>
2026-06-19 12:32:27 +0530

    server 2nd

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 6700;
+const PORT = process.env.PORT || 6700;
 
 // to tell express where static files - html, css, js - are located
 app.use(express.static("frontend"));
-app.listen(port,function(){
-    console.log("App is running on https://localhost:" + port);
+app.listen(PORT, function(){
+    console.log("App is running on http://localhost:" + PORT);
 });
